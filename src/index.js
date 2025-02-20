a6348eb241451956448367ad243fc5653c37a052
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2025-02-20 15:31:27 +0530

    added creation date and time

--- src/index.js
+++ src/index.js
@@ -22,7 +22,14 @@ exports.handler = async (event) => {
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'Reviews seeded successfully',reviews, reviewCount: reviews.length }),
+            body: JSON.stringify({ message: 'Reviews seeded successfully',
+                                  reviewCount: reviews.length,
+                                  createdAt:{
+                                    date: new Date().tolocaleDateString(),
+                                    time: new Date().toLocaleTimeString()
+                                  } ,
+                                  reviews, 
+                                }),
         };
     } catch (error) {
         console.error('Error:', error);
